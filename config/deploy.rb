9728179c9e991fc9710307726e513959b6afd3d4
Joseph Landry <wjlandryiii@gmail.com>
2013-12-03 21:22:37 -0800

    fixing errors with bundler in production

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,4 @@
+require 'bundler/capistrano'
 #server 'appserver.home', roles: [:web, :app, :db]
 
 set :application, 'hashdb'
@@ -26,7 +27,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
+      # execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
