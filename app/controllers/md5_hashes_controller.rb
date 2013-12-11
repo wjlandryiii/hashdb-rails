42f35df9053646cf0a1e14a1194b8b0d3b618498
Joseph Landry <wjlandryiii@gmail.com>
2013-12-10 19:26:30 -0800

    got eharmony.txt to import in ~60 seconds

--- app/controllers/md5_hashes_controller.rb
+++ app/controllers/md5_hashes_controller.rb
@@ -3,7 +3,7 @@ require 'pager'
 
 class Md5HashesController < ApplicationController
 	def index
-		@md5hashes = Md5Hash.paginate(:page => params[:page])
+		@md5hashes = Md5Hash.paginate(:page => params[:page]).order(:hex_hash)
 
 		# @pager = Pager.new(Md5Hash.count(), params[:page], 15)
 
@@ -99,38 +99,54 @@ class Md5HashesController < ApplicationController
 		redirect_to md5_hashes_path
 	end
 
+	# def upload_passwords
+	# 	passwordCount = 0
+	# 	solvedHashCount = 0
+	# 	novelPasswordCount = 0
+	# 	novelHashCount = 0
+	# 	upload = params[:upload]
+	# 	datafile = upload[:datafile]
+	# 	tmpfile = datafile.tempfile
+	# 	while(password = tmpfile.gets)
+	# 		passwordCount += 1
+	# 		password.force_encoding('UTF-8')
+	# 		password.chomp!
+	# 		md5hash = Md5Hash.find_by_password(password)
+	# 		if !md5hash
+	# 			novelPasswordCount += 1
+	# 			hex_hash = Digest::MD5.hexdigest(password)
+	# 			md5hash = Md5Hash.find_by_hex_hash(hex_hash)
+	# 			if md5hash
+	# 				solvedHashCount += 1
+	# 				md5hash.password = password
+	# 				md5hash.save
+	# 			else
+	# 				novelHashCount += 1
+	# 				md5hash = Md5Hash.new
+	# 				md5hash.hex_hash = hex_hash
+	# 				md5hash.password = password
+	# 				md5hash.save
+	# 			end
+	# 		end
+	# 	end
+	# 	flash[:notice] = passwordCount.to_s() + " passwords" + " submitted and " + novelHashCount.to_s() + " were novel.  " + novelHashCount.to_s() + "  novel hashes were added and " + solvedHashCount.to_s() + " known hashes were solved."
+	# 	redirect_to md5_hashes_path
+	# end
+
 	def upload_passwords
-		passwordCount = 0
-		solvedHashCount = 0
-		novelPasswordCount = 0
-		novelHashCount = 0
 		upload = params[:upload]
-		datafile = upload[:datafile]
-		tmpfile = datafile.tempfile
-		while(password = tmpfile.gets)
-			passwordCount += 1
-			password.force_encoding('UTF-8')
-			password.chomp!
-			md5hash = Md5Hash.find_by_password(password)
-			if !md5hash
-				novelPasswordCount += 1
-				hex_hash = Digest::MD5.hexdigest(password)
-				md5hash = Md5Hash.find_by_hex_hash(hex_hash)
-				if md5hash
-					solvedHashCount += 1
-					md5hash.password = password
-					md5hash.save
-				else
-					novelHashCount += 1
-					md5hash = Md5Hash.new
-					md5hash.hex_hash = hex_hash
-					md5hash.password = password
-					md5hash.save
-				end
-			end
+		if upload != nil
+			t1 = Time.now.to_f
+			
+			count = Md5Hash.import_passwords_from_file upload[:datafile].path
+
+			t2 = Time.now.to_f
+			flash[:notice] = count.to_s + " password submitted. " + (t2-t1).to_s + " seconds."
+			redirect_to md5_hashes_path
+		else
+			flash[:error] = "No password file selected"
+			render 'new'
 		end
-		flash[:notice] = passwordCount.to_s() + " passwords" + " submitted and " + novelHashCount.to_s() + " were novel.  " + novelHashCount.to_s() + "  novel hashes were added and " + solvedHashCount.to_s() + " known hashes were solved."
-		redirect_to md5_hashes_path
 	end
 
 	def show
@@ -145,37 +161,57 @@ class Md5HashesController < ApplicationController
 	end
 
 
-
-	def unsolved
-		t1 = Time.now.to_f
-
+	def unsolved_a
 		connection = ActiveRecord::Base.connection()
 		sql = "SELECT hex_hash FROM md5_hashes WHERE password IS NULL"
 		results = connection.execute(sql)
 		unsolved = results.map { |x| x["hex_hash"]}
 		txt = unsolved.join("\r\n")
-
-		t2 = Time.now.to_f
-		#txt +=  (t2-t1).to_s + "\r\n"
-
 		render text: txt, content_type:"text/plain"
 	end
 
-	def wordlist
+  def unsolved_b #slightly faster and doesnt have to render
+    file = Tempfile.new("unsolved")
+    Rails.logger.debug(file)
+    Md5Hash.export_unsolved_hashes_to_file(file)
+    send_file file.path, :type => 'text/plain', :filename => "unsolved.txt"
+  end
+
+	def unsolved
 		t1 = Time.now.to_f
 
-		connection = ActiveRecord::Base.connection()
-		sql = "SELECT password FROM md5_hashes WHERE password IS NOT NULL"
-		results = connection.execute(sql)
-		passwords = results.map { |x| x["password"]}
-		txt = passwords.join("\r\n")
+    unsolved_b
 
 		t2 = Time.now.to_f
 
-		#txt +=  (t2-t1).to_s + "\r\n"
-		render text: txt, content_type:"text/plain"
+    Rails.logger.debug("!!!!! Export time: " + (t2 - t1).to_s + " seconds")
+	end
 
 
+  def wordlist_a
+    connection = ActiveRecord::Base.connection()
+    sql = "SELECT password FROM md5_hashes WHERE password IS NOT NULL"
+    results = connection.execute(sql)
+    passwords = results.map { |x| x["password"]}
+    txt = passwords.join("\r\n")
+
+    render text: txt, content_type:"text/plain"
+  end
+
+  def wordlist_b
+    file = Tempfile.new("wordlist")
+    Rails.logger.debug(file)
+    Md5Hash.export_passwords_to_file(file)
+    send_file file.path, :type => 'text/plain', :filename => "wordlist.txt"
+  end
+
+	def wordlist
+    t1 = Time.now.to_f
+
+    wordlist_b
+
+    t2 = Time.now.to_f
+    Rails.logger.debug("!!!!! Export time: " + (t2 - t1).to_s + " seconds")
 	end
 
 	def stats
--- app/models/md5_hash.rb
+++ app/models/md5_hash.rb
@@ -49,6 +49,41 @@ class Md5Hash < ActiveRecord::Base
       }
     end
 
+    def import_hashes_C(hashes)
+      hashesSet = Set.new(hashes)
+      knownHashesSet = Set.new()
+
+      connection = ActiveRecord::Base.connection.raw_connection
+      connection.prepare("my_hash_findall_by_hex_hash", "SELECT id, hex_hash, password FROM md5_hashes WHERE hex_hash IN ($1)")
+
+
+      connection.exec("DEALLOCATE my_hash_findall_by_hex_hash")
+    end
+
+    def import_hashes_D(hashes)
+      connection = ActiveRecord::Base.connection.raw_connection
+      hashes_csv = hashes.join("\r\n")
+      connection.transaction do
+        connection.exec( "COPY md5_hashes (hex_hash) FROM STDIN WITH csv" )
+        connection.put_copy_data(hashes_csv)
+        connection.put_copy_end
+      end
+    end
+
+    def import_hashes_E(hashes) #this is the ticket.  Imports eharmony.txt in about 60 seconds
+      connection = ActiveRecord::Base.connection.raw_connection
+      hashes_csv = hashes.join("\r\n")
+
+      connection.transaction do
+        connection.exec("CREATE TEMPORARY TABLE tmp_hashes (hex_hash varchar(64)) ON COMMIT DROP")
+        #connection.exec("CREATE UNIQUE INDEX tmp_hashes_hex_hash_index ON tmp_hashes (hex_hash)")  #this index does not inprove proformance
+        connection.exec("COPY tmp_hashes (hex_hash) FROM STDIN WITH csv" )
+        connection.put_copy_data(hashes_csv)
+        connection.put_copy_end
+        connection.exec("INSERT INTO md5_hashes (hex_hash) SELECT t.hex_hash FROM tmp_hashes t LEFT JOIN md5_hashes h ON h.hex_hash = t.hex_hash WHERE h.hex_hash IS NULL")
+      end
+
+    end
 
     def import_from_file(file)
       count = 0
@@ -66,8 +101,148 @@ class Md5Hash < ActiveRecord::Base
       hashes = hashes.to_a()
       hashes.sort!
 
-      Md5Hash.import_hashes_B(hashes)
+      t1 = Time.now.to_f
+      Md5Hash.import_hashes_E(hashes)
+      t2 = Time.now.to_f
+      Rails.logger.debug("!!!!! import time: " + (t2 - t1).to_s + "seconds")
       count
+    end
+
+    def export_unsolved_hashes_to_file(file)
+      connection = ActiveRecord::Base.connection.raw_connection
+      buff = ''
+      connection.transaction do
+        connection.exec "COPY (SELECT hex_hash FROM md5_hashes WHERE password IS NULL) TO STDOUT CSV" 
+        file.write(buff) while buff = connection.get_copy_data()
+      end
+      file.flush()
+    end
+
+    def import_passwords_naive_solution(passwords)
+      passwords.each do | password |
+        hash = Digest::MD5.hexdigest(password)
+
+        md5hash = Md5Hash.find_by_hex_hash hash
+        if md5hash
+          md5hash.password = password
+          md5hash.save
+        else
+          md5hash = Md5Hash.new
+          md5hash.hex_hash = hash
+          md5hash.password = password
+          md5hash.save
+        end
+      end
+    end
+
+    def import_passwords_naive_solution2(passwords)
+      connection = ActiveRecord::Base.connection.raw_connection
+
+      connection.prepare("my_hash_lookup", "SELECT id, hex_hash, password FROM md5_hashes WHERE hex_hash = $1 LIMIT 1")
+      connection.prepare("my_hash_update", "UPDATE md5_hashes SET password = $1 WHERE hex_hash = $2")
+      connection.prepare("my_hash_create", "INSERT INTO md5_hashes (hex_hash, password) VALUES ($1, $2)")
+
+      passwords.each do | password |
+        hash = Digest::MD5.hexdigest(password)
+        
+        hr = connection.exec_prepared("my_hash_lookup", [hash])
+        if hr.first != nil
+          if hr[0][:password] == nil
+            connection.exec_prepared("my_hash_update", [password, hash])
+          end
+        else
+          connection.exec_prepared("my_hash_create", [hash, password])
+        end
+      end
+
+      connection.exec("DEALLOCATE my_hash_lookup")
+      connection.exec("DEALLOCATE my_hash_update")
+      connection.exec("DEALLOCATE my_hash_create")
+    end
+
+    def import_passwords_naive_solution3(passwords)
+
+      passwordSet = Set.new(passwords)
+      connection = ActiveRecord::Base.connection.raw_connection
+
+      connection.prepare("my_hash_lookup", "SELECT id, hex_hash, password FROM md5_hashes WHERE hex_hash = $1 LIMIT 1")
+      connection.prepare("my_hash_findall_by_password", "SELECT id, hex_hash, password FROM md5_hashes WHERE password IN ($1)")
+      connection.prepare("my_hash_findall_by_hex_hash", "SELECT id, hex_hash, password FROM md5_hashes WHERE hex_hash IN ($1)")
+      connection.prepare("my_hash_update", "UPDATE md5_hashes SET password = $1 WHERE hex_hash = $2")
+      connection.prepare("my_hash_create", "INSERT INTO md5_hashes (hex_hash, password) VALUES ($1, $2)")
+      connection.prepare("my_hash_create_bulk", "INSERT INTO md5_hashes (hex_hash, password) VALUES (($1,$2))")
+
+      hr = connection.exec_prepared("my_hash_findall_by_password", [passwordSet.to_a()])
+
+      hr.each do | row |
+        passwordSet.delete(row[:password])
+      end
+
+      passwords = passwordSet.to_a()
+
+      hashes = {}
+
+      allHashes = Set.new()
+      passwords.each do | password |
+        hash = Digest::MD5.hexdigest(password).downcase
+        hashes[hash] = password
+        allHashes.add(hash)
+      end
+
+      updateHashes = Set.new()
+      hr = connection.exec_prepared("my_hash_findall_by_hex_hash", [hashes.keys])
+      hr.each do | row |
+        updateHashes.add(hr[:hex_hash])
+      end
+
+      insertHashes = allHashes.subtract(updateHashes)
+
+      insertTuples = insertHashes.to_a.map { | hash | [hash, hashes[hash]]}
+
+      connection.exec_prepared("my_hash_create_bulk", [insertTuples])
+
+      updateHashes.each do | hash |
+        connection.exec_prepared("my_hash_update", [hashes[hash], hash])
+      end
+
+
+      connection.exec("DEALLOCATE my_hash_lookup")
+      connection.exec("DEALLOCATE my_hash_findall_by_password")
+      connection.exec("DEALLOCATE my_hash_findall_by_hex_hash")
+      connection.exec("DEALLOCATE my_hash_update")
+      connection.exec("DEALLOCATE my_hash_create")
+      connection.exec("DEALLOCATE my_hash_create_bulk")
+    end
+
+
+    def import_passwords(passwords)
+      Md5Hash.import_passwords_naive_solution2(passwords)
+    end
+
+    def import_passwords_from_file(fileName)
+      count = 0
+      file = File.open(fileName, "r:UTF-8")
+      Rails.logger.debug("File: " + file.to_s)
+      passwords = Set.new
+      while(password = file.gets)
+        password.chomp!
+        count += 1
+        passwords.add(password)
+      end
+      Rails.logger.debug("Password Count: " + count.to_s)
+      file.close
+      Md5Hash.import_passwords(passwords.to_a)
+      count
+    end
+
+    def export_passwords_to_file(file)
+      connection = ActiveRecord::Base.connection.raw_connection
+      buff = ''
+      connection.transaction do
+        connection.exec "COPY (SELECT password FROM md5_hashes WHERE password IS NOT NULL) TO STDOUT CSV" 
+        file.write(buff) while buff = connection.get_copy_data()
+      end
+      file.flush()
     end
   end
 end
